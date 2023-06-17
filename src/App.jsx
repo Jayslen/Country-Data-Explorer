292211f1bf53296e57ababd21db5fd17e1aed176
Jayslen Laptop <jayslen134@gmail.com>
2023-06-16 22:39:06 -0400

    header fixed

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App () {
         </div>
       </header>
       <main className='px-14 mt-6' style={{ color: elmText }}>
-        <header className='flex justify-between items-center'>
+        <header className='flex flex-col gap-4 justify-between items-center md:gap-0 md:flex-row'>
           <input type='text' placeholder='Search for a country' className='font-Nunito p-2 rounded-sm' style={{ backgroundColor: elmColor }} onChange={handleChange} />
           <Filter update={handleClick} color={elmColor} />
         </header>
