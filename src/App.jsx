42c1927174990faa28811d2501d28b6466cdc93f
Jayslen <jayslen134@gmail.com>
2023-06-17 23:04:11 -0400

    fixing erorrs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { IconSunFilled, IconMoonFilled } from '@tabler/icons-react'
 import useGetData from './hooks/useDataCountry'
 import { useCountryData } from './hooks/useSingleCountryData'
 import { Country } from './components/CountryInfo'
@@ -7,6 +6,7 @@ import { Loading } from './components/Loading'
 import { useTheme } from './hooks/useTheme'
 import { FullPage } from './components/FullPageCountry'
 import { useState } from 'react'
+import { Header } from './components/HeroHeader'
 
 window.document.body.style.backgroundColor = '#202d36'
 function App () {
@@ -39,16 +39,7 @@ function App () {
 
   return (
     <>
-      <header className='flex justify-between items-center p-5 shadow-xl relative z-20' style={{ backgroundColor: elmColor, color: elmText }}>
-        <h1 className='text-xl font-bold'>Where in the wordl?</h1>
-        <div className='flex gap-1'>
-          {darkMode ? <IconSunFilled /> : <IconMoonFilled />}
-          <p className='font-semibold cursor-pointer' onClick={handleTheme}>{darkMode
-            ? 'Light Mode'
-            : 'Dark Mode'}
-          </p>
-        </div>
-      </header>
+  <Header elmColor={elmColor} handleTheme={handleTheme} elmText={elmText} darkMode={darkMode}/>
       <main className='px-14 mt-6' style={{ color: elmText , height:`${open ? '50vh' : 'full'}`, overflow: `${open ? 'hidden': 'visible'}` }}>
         <header className='flex flex-col gap-4 justify-between items-center md:gap-0 md:flex-row'>
           <input type='text' placeholder='Search for a country' className='font-Nunito p-2 rounded-sm' style={{ backgroundColor: elmColor }} onChange={handleChange} />
--- src/components/FullPageCountry.jsx
+++ src/components/FullPageCountry.jsx
@@ -1,19 +1,14 @@
-import data from '../mocks/single-country.json'
 import { Borders, List } from './fullpage/CountryDataLists'
-
-
-
 
 export function FullPage ({bodyColor, bgColor, country, textColor, goBack }) {
   return (
-    <section className='w-screen h-screen grid place-content-center absolute right-0 top-0 px-20' style={{ backgroundColor: bodyColor , color : textColor}}>
-
+    <section className='w-screen h-screen absolute grid place-content-center right-0 top-0 px-20' style={{ backgroundColor: bodyColor , color : textColor}}>
       {country?.map(data => {
 
         return (
-          <div className='w-[90vw] h-auto flex flex-col mt-16 md:flex-row md:justify-center md:gap-20 relative' key={data.name}>
+          <div className='w-[90vw] h-auto flex flex-col mt-28 md:mt-0 md:flex-row md:justify-center md:gap-20 relative' key={data.name}>
 
-            <figure className='max-w-[400px] relative self-center'>
+            <figure className='max-w-[400px] relative'>
               <img src={data.flag} alt={data.imageAlt} className='w-full' />
             <button className='absolute -top-14 left-0 px-4 py-2 cursor-pointer transition-all hover:brightness-75' style={{backgroundColor: bgColor}} onClick={goBack}>Go back</button>
             </figure>
